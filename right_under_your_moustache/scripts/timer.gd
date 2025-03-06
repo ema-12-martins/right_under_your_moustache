486f1c332668b6dae69c5ef766aeb415d8ddbdae
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-06 22:50:36 +0000

    esta a funcionar

--- right_under_your_moustache/scripts/timer.gd
+++ right_under_your_moustache/scripts/timer.gd
@@ -4,5 +4,6 @@ extends Timer
 @onready var collision_shape: CollisionShape2D = augusto.get_node("CollisionShape2D")
 
 func _on_timeout() -> void:
+	print("eserei")
 	# Reabilita a colisão após o movimento
 	collision_shape.disabled = false
