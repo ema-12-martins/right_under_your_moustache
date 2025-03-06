589986ce63b4ab035551a5e6700df35b457e1ff0
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-06 20:03:00 +0000

    a gaja nao esta ajudar a descer

--- right_under_your_moustache/scripts/test.gd
+++ right_under_your_moustache/scripts/test.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 @onready var interactable: Area2D = $Interactable
 @onready var augusto: CharacterBody2D = $"../../augusto"
+@onready var collision_shape: CollisionShape2D = augusto.get_node("CollisionShape2D")
 
 var level = 1
 
@@ -10,11 +11,18 @@ func _ready() -> void:
 	interactable.interact = _on_interact
 
 func _on_interact(key_pressed: String):
-	print("Key pressed: "+key_pressed)
-	
-	if level == 0 and key_pressed=="W":
+	print("Key pressed: " + key_pressed)
+
+	if level == 0 and key_pressed == "W":
+		# Desabilita a colisão durante o movimento
+		collision_shape.disabled = true
 		augusto.position.y -= 800
 		level += 1
-	elif level == 1 and key_pressed=="S":
+		# Reabilita a colisão após o movimento
+		collision_shape.disabled = false
+		
+	elif level == 1 and key_pressed == "S":
+		collision_shape.disabled = true
 		augusto.position.y += 800
 		level -= 1
+		collision_shape.disabled = false
