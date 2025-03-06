3a11c096d6314b817bbdcd8ba8d96e65a31a0080
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-06 19:47:42 +0000

    logica movimentos elevadores

--- right_under_your_moustache/scripts/test.gd
+++ right_under_your_moustache/scripts/test.gd
@@ -1,6 +1,9 @@
 extends StaticBody2D
 
 @onready var interactable: Area2D = $Interactable
+@onready var augusto: CharacterBody2D = $"../../augusto"
+
+var level = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,3 +11,10 @@ func _ready() -> void:
 
 func _on_interact(key_pressed: String):
 	print("Key pressed: "+key_pressed)
+	
+	if level == 0 and key_pressed=="W":
+		augusto.position.y -= 800
+		level += 1
+	elif level == 1 and key_pressed=="S":
+		augusto.position.y += 800
+		level -= 1
