9083c493964415f97028874277a0f4a7d54f03bd
IgorAndrade10 <igor-m-andrade@hotmail.com>
2025-03-06 22:45:02 +0000

    enemy

---
+++ right_under_your_moustache/scripts/senhora_limpeza.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+
+@export var speed = 100.0
+@export var patrol_points: Array[Marker2D] = []
+var current_point_index = 0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	if patrol_points.size() == 0:
+		print("No patrol points assigned!")
+
+func _process(delta: float) -> void:
+	patrol()
+	move_and_slide()
+
+func patrol() -> void:
+	if patrol_points.size() > 0:
+		animated_sprite.play("run")
+		var target = patrol_points[current_point_index].position
+		velocity = (target - position).normalized() * speed
+		if velocity.x < 0:
+			animated_sprite.flip_h = true
+		else:
+			animated_sprite.flip_h = false
+		
+		if position.distance_to(target) < 10.0:
+			current_point_index += 1
+			if current_point_index >= patrol_points.size():
+				current_point_index = 0
+
+func _draw():
+	for point in patrol_points:
+		draw_circle(point.position, 5, Color(1, 0, 0))  # Red circles
