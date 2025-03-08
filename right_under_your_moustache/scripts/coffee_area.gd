5e4b5a1a98169d64fdfeb644f200cdc909f6a34a
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-08 19:05:50 +0000

    + do mesmo

--- right_under_your_moustache/scripts/coffee_area.gd
+++ right_under_your_moustache/scripts/coffee_area.gd
@@ -1,4 +1,10 @@
 extends Area2D
 
+@onready var label: Label = $"../Label"
+
 func _on_body_entered(body: Node2D) -> void:
+	label.visible = true
 	print("ola")
+	
+func _on_body_exited(body: Node2D) -> void:
+	label.visible = false
