be430bdd068360844f860867987c457d64697697
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-09 01:51:09 +0000

    ja mexe no mesmo sitio

--- right_under_your_moustache/scripts/camera.gd
+++ right_under_your_moustache/scripts/camera.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var area_2d: Area2D = $Area2D
 @onready var polygon_2d: Polygon2D = $Polygon2D
 
+var local_equal_inicial = true
+
 func _ready() -> void:
 	area_2d.body_entered.connect(_on_body_entered)
 	
@@ -17,3 +19,12 @@ func reload_scene():
 func _on_flip_camera_timeout() -> void:
 	area_2d.scale.x *= -1
 	polygon_2d.scale.x *= -1
+	
+	if local_equal_inicial == true:
+		local_equal_inicial = false
+		area_2d.position.x -= 180
+		polygon_2d.position.x -= 180
+	elif local_equal_inicial == false:
+		local_equal_inicial = true
+		area_2d.position.x += 180
+		polygon_2d.position.x += 180
