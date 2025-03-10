21abb948440d430dd8138d155ba0dfabf077be9d
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-10 11:44:14 +0000

    miuda em baixo

--- right_under_your_moustache/scripts/levels.gd
+++ right_under_your_moustache/scripts/levels.gd
@@ -1,3 +1,10 @@
 extends Node
 
-var level = 1
+var level = 0
+@onready var collision_shape_level_0: CollisionShape2D = $level_0/CollisionShape2D
+@onready var collision_shape_level_1: CollisionShape2D = $level_1/CollisionShape2D
+
+
+func _ready() -> void:
+	collision_shape_level_0.disabled = false
+	collision_shape_level_1.disabled = true
