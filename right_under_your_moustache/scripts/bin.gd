0d13b95acad8742aac63a9b8d8276bdc1fd0b15c
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-11 01:10:51 +0000

    so interage com o caixote do lixo ate encontrar a chave

--- right_under_your_moustache/scripts/bin.gd
+++ right_under_your_moustache/scripts/bin.gd
@@ -3,12 +3,20 @@ extends Area2D
 @onready var label: Label = $"../Label"
 
 var key_founded = false
+var inside_area = false
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "augusto":
+	if body.name == "augusto" and key_founded == false:
 		label.visible = true
-		Global.keys += 1
+		inside_area = true
 
 func _on_body_exited(body: Node2D) -> void:
-	if body.name == "augusto":
+	if body.name == "augusto" and key_founded == false:
+		label.visible = false
+		inside_area = false
+
+func _process(_delta: float) -> void:
+	if inside_area and Input.is_action_just_pressed("ui_accept"):  # 'ui_accept' == Enter
+		Global.keys += 1
+		key_founded = true
 		label.visible = false
