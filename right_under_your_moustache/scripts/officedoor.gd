47673193941e0b8c3981a6d01545254f7b622e3b
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-11 02:16:41 +0000

    finalmente abre a porta com a chave

--- right_under_your_moustache/scripts/officedoor.gd
+++ right_under_your_moustache/scripts/officedoor.gd
@@ -1,20 +1,25 @@
 extends StaticBody2D
 
 @onready var interactable: Area2D = $Interactable
+@onready var interactable_2: Area2D = $Interactable2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interactable.interact = _on_interact
-	
-func _process(delta: float) -> void:
-	if Global.keys == 1:
-		interactable.name = "Press Enter to enter the office."
-	else: 
-		interactable.name = "Find the key to unlock this door."
+	interactable_2.interact = _on_interact
 		
 func _on_interact(key_pressed: String):
 	if Global.keys == 1:
 		if key_pressed == "Enter":
-			Global.last_world_position = $"../augusto".position
+			Global.last_world_position = $"../../augusto".position
 			get_tree().change_scene_to_file("res://scenes/office.tscn")
 	
+
+
+func _on_interactable_body_entered(body: Node2D) -> void:
+	if Global.keys == 1:
+		interactable.visible = true
+		interactable_2.visible = false
+	else: 
+		interactable.visible = false
+		interactable_2.visible = true
