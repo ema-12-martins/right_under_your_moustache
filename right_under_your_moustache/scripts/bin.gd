3c034b32a64a48bf4be6ad756d41e24e47a78971
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-11 01:24:40 +0000

    so falta mudar name

--- right_under_your_moustache/scripts/bin.gd
+++ right_under_your_moustache/scripts/bin.gd
@@ -16,7 +16,7 @@ func _on_body_exited(body: Node2D) -> void:
 		inside_area = false
 
 func _process(_delta: float) -> void:
-	if inside_area and Input.is_action_just_pressed("ui_accept"):  # 'ui_accept' == Enter
+	if inside_area and Input.is_action_just_pressed("ui_accept") and key_founded==false:  # 'ui_accept' == Enter
 		Global.keys += 1
 		key_founded = true
 		label.visible = false
--- right_under_your_moustache/scripts/officedoor.gd
+++ right_under_your_moustache/scripts/officedoor.gd
@@ -5,8 +5,16 @@ extends StaticBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interactable.interact = _on_interact
-
+	
+func _process(delta: float) -> void:
+	if Global.keys == 1:
+		interactable.name = "Press Enter to enter the office."
+	else: 
+		interactable.name = "Find the key to unlock this door."
+		
 func _on_interact(key_pressed: String):
-	if key_pressed == "Enter":
-		Global.last_world_position = $"../augusto".position
-		get_tree().change_scene_to_file("res://scenes/office.tscn")
+	if Global.keys == 1:
+		if key_pressed == "Enter":
+			Global.last_world_position = $"../augusto".position
+			get_tree().change_scene_to_file("res://scenes/office.tscn")
+	
