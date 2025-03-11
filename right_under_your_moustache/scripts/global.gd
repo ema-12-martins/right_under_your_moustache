7a32c6aa627dcdbbf401fe00dafb8c62a6669a51
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-11 20:40:50 +0000

    timer

--- right_under_your_moustache/scripts/global.gd
+++ right_under_your_moustache/scripts/global.gd
@@ -3,6 +3,7 @@ extends Node
 var last_world_position = Vector2(0, 0)
 var last_office_position = Vector2(0, 0)
 var got_exam = false
+var elipsed_time_in_seconds = 0
 
 signal keys_updated  # Sinal para notificar mudanças na variável keys
 
