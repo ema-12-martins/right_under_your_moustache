4360a7095c593551e18eead59c1616d1b258ef86
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-11 20:43:27 +0000

    menos 1 bug mas ha mais

--- right_under_your_moustache/scripts/run_time.gd
+++ right_under_your_moustache/scripts/run_time.gd
@@ -4,6 +4,9 @@ extends Timer
 
 func _ready() -> void:
 	run_timer.start()
+	var minutes = Global.elipsed_time_in_seconds/60
+	var seconds = Global.elipsed_time_in_seconds % 60
+	current_time.text = "%02d:%02d" % [minutes, seconds]
 	
 func _on_timeout() -> void:
 	Global.elipsed_time_in_seconds += 1
