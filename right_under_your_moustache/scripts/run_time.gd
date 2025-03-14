9a5a17debc50612ed2e9588ca66013b3861ea6f5
IgorAndrade10 <igor-m-andrade@hotmail.com>
2025-03-14 15:22:22 +0000

    nem sei qual é o conflito honestamente

---
+++ right_under_your_moustache/scripts/run_time.gd
@@ -0,0 +1,16 @@
+extends Timer
+@onready var run_timer: Timer = $"."
+@onready var current_time: Label = $"../current_time"
+
+func _ready() -> void:
+	run_timer.start()
+	var minutes = Global.elipsed_time_in_seconds/60
+	var seconds = Global.elipsed_time_in_seconds % 60
+	current_time.text = "%02d:%02d" % [minutes, seconds]
+	
+func _on_timeout() -> void:
+	Global.elipsed_time_in_seconds += 1
+	var minutes = Global.elipsed_time_in_seconds/60
+	var seconds = Global.elipsed_time_in_seconds % 60
+	current_time.text = "%02d:%02d" % [minutes, seconds]
+	run_timer.start()
