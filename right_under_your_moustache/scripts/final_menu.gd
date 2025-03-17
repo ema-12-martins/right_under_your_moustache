0a65b0d5f2e7d30566d187cb74ff7a887b04abe7
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-17 22:08:46 +0000

    botoes

--- right_under_your_moustache/scripts/final_menu.gd
+++ right_under_your_moustache/scripts/final_menu.gd
@@ -16,22 +16,66 @@ func _ready():
 	$VBoxContainer/exit.connect("mouse_entered", Callable(self, "_on_mouse_entered").bind($VBoxContainer/exit))
 
 func _on_restart_pressed() -> void:
+	#Create sound interacting
+	var audio_player = AudioStreamPlayer2D.new()
+	audio_player.stream = preload("res://music/button_short.mp3")  
+	audio_player.bus = "Master"  
+	audio_player.global_position = global_position 
+	
+	audio_player.max_distance = 10_000_000
+	
+	get_parent().add_child(audio_player)
+	audio_player.play()
+	
 	Global.global_scene_instance = preload("res://scenes/hud.tscn").instantiate()
 	Global.replay()
 
 func _on_exit_pressed() -> void:
+	#Create sound interacting
+	var audio_player = AudioStreamPlayer2D.new()
+	audio_player.stream = preload("res://music/button_short.mp3")  
+	audio_player.bus = "Master"  
+	audio_player.global_position = global_position 
+	
+	audio_player.max_distance = 10_000_000
+	
+	get_parent().add_child(audio_player)
+	audio_player.play()
+	
 	get_tree().quit()
 
 func _on_mouse_entered(button) -> void:
 	button.grab_focus()
 
 func _on_restart_focus_entered() -> void:
+	#Create sound interacting
+	var audio_player = AudioStreamPlayer2D.new()
+	audio_player.stream = preload("res://music/button_short.mp3")  
+	audio_player.bus = "Master"  
+	audio_player.global_position = global_position 
+	
+	audio_player.max_distance = 10_000_000
+	
+	get_parent().add_child(audio_player)
+	audio_player.play()
+	
 	$VBoxContainer/restart.add_theme_color_override("font_hover_color", Color("#ffff4c"))
 
 func _on_restart_focus_exited() -> void:
 	$VBoxContainer/restart.add_theme_color_override("font_hover_color", Color("#FFFFFF"))
 
 func _on_exit_focus_entered() -> void:
+	#Create sound interacting
+	var audio_player = AudioStreamPlayer2D.new()
+	audio_player.stream = preload("res://music/button_short.mp3")  
+	audio_player.bus = "Master"  
+	audio_player.global_position = global_position 
+	
+	audio_player.max_distance = 10_000_000
+	
+	get_parent().add_child(audio_player)
+	audio_player.play()
+	
 	$VBoxContainer/exit.add_theme_color_override("font_hover_color", Color("#ffff4c"))
 
 func _on_exit_focus_exited() -> void:
