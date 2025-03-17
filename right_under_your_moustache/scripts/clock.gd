f392df0455d2aa5e4af5ecf9808ca64495a7d4cd
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-17 16:11:11 +0000

    falta ajustar letras e assim mas nada demais

--- right_under_your_moustache/scripts/clock.gd
+++ right_under_your_moustache/scripts/clock.gd
@@ -10,8 +10,22 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		label.visible = true
 
 func _process(_delta: float) -> void:
-	if inside_area and Input.is_action_just_pressed("ui_accept"):  # 'ui_accept' == Enter
+	if inside_area and Input.is_action_just_pressed("ui_accept"):  # 'ui_accept' == Enter key
 		Global.time_mutex.lock()
 		Global.elipsed_time_in_seconds -= 10
 		Global.time_mutex.unlock()
+		
+		# Create a label to display "-10s"
+		var time_label = Label.new()
+		time_label.text = "-10s"
+		time_label.add_theme_color_override("font_color", Color.RED) 
+		time_label.add_theme_font_size_override("font_size", 36)
+		time_label.set_position(global_position + Vector2(0, -150)) 
+		get_parent().add_child(time_label) 
+		
+		#Remove clock
 		queue_free()
+		
+		# Wait 3 seconds before removing the label
+		await get_tree().create_timer(1).timeout  
+		time_label.queue_free() 
