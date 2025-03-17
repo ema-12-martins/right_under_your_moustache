021eab2a13c4b4e9c038068bd3e227f188693050
ema-12-martins <a97678@alunos.uminho.pt>
2025-03-17 21:57:17 +0000

    subir e descer escadas

--- right_under_your_moustache/scripts/test.gd
+++ right_under_your_moustache/scripts/test.gd
@@ -15,9 +15,38 @@ func _ready() -> void:
 func _on_interact(key_pressed: String):
 
 	if levels.level == 0 and (key_pressed == "W" or key_pressed == "Up"):
+		#Create sound interacting
+		var audio_player = AudioStreamPlayer2D.new()
+		audio_player.stream = preload("res://music/stairs_short.mp3")  
+		audio_player.bus = "Master"  
+		audio_player.global_position = global_position 
+		
+		audio_player.max_distance = 10_000_000
+		
+		get_parent().add_child(audio_player)
+		audio_player.play()
+		
+		#Time for the sound to work
+		augusto.visible=false
+		await get_tree().create_timer(1).timeout 
+		augusto.visible=true
+		
 		augusto.position.y -= 700
 		levels.level += 1
 	
 	elif levels.level == 1 and (key_pressed == "S" or key_pressed == "Down"):
+		#Create sound interacting
+		var audio_player = AudioStreamPlayer2D.new()
+		audio_player.stream = preload("res://music/stairs_short.mp3")  
+		audio_player.bus = "Master"  
+		audio_player.global_position = global_position 
+		
+		audio_player.max_distance = 10_000_000
+		
+		get_parent().add_child(audio_player)
+		audio_player.play()
+		
+		await get_tree().create_timer(1).timeout 
+		
 		augusto.position.y += 800
 		levels.level -= 1
