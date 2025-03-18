8dc1f2d65b5cfef34a73de59aef61dac56db7ce7
IgorAndrade10 <igor-m-andrade@hotmail.com>
2025-03-18 00:12:03 +0000

    mudança na saida

--- right_under_your_moustache/scripts/game.gd
+++ right_under_your_moustache/scripts/game.gd
@@ -8,3 +8,5 @@ func _initialize() -> void:
 		$Door/OfficeDoor/Interactable.interact_name = "Press Enter to enter the office"
 	if Global.last_world_position != Vector2(0, 0):
 		$augusto.position = Global.last_world_position
+	if Global.got_exam == 1:
+		$Exit/Interactable.interact_name = "Press Enter to exit"
